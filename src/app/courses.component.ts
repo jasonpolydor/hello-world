c1577e9d4cdf15cc5c2d4c40a59ab85cf60f1867
jasonpolydor <jasonpolydor@gmail.com>
2018-03-25 20:58:13 +0400

    Udemy property and attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,6 +5,12 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   template: `
   <h2>{{ title }}</h2>
+  <img [src] = "imgUrl" />
+  <table>
+      <tr>
+          <td [attr.colspan]="colSpan"></td>
+      </tr>
+  </table>
   <ul>
     <li *ngFor="let course of courses">{{ course }}</li>
   </ul>
@@ -12,7 +18,9 @@ import { CoursesService } from './courses.service';
 })
 export class CoursesComponent {
     title = "List of courses";
+    imgUrl = "https://dummyimage.com/600x400/000/fff"; //e.g. of one way property binding
     courses;
+    colSpan = 2;
 
     constructor(service: CoursesService){
       this.courses = service.getCourses();
